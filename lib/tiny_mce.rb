ff09e3700e5e0d9a469a0c69681b32ba4827f0ff
Kieran Pilkington <kieran@katipo.co.nz>
2008-10-29 14:30:14 +1300

    enhancement: cleaned up new spellchecking functionality, added multi OS support, updated documentation, removed old alias methods (it quick to update to the new methods), and use each_with_index

--- lib/tiny_mce.rb
+++ lib/tiny_mce.rb
@@ -1,90 +1,92 @@
-require 'net/https'
-require 'uri'
-require 'rexml/document'
-  
 module TinyMCE
-  module ClassMethods
-		ASPELL_WORD_DATA_REGEX = Regexp.new(/\&\s\w+\s\d+\s\d+(.*)$/)
-	  ASPELL_PATH = "aspell"
-    
-    def uses_tiny_mce(options = {})
-      tiny_mce_options = (options.delete(:options) || {}).clone
+  def self.included(base)
+    base.extend(ClassMethods)
+    base.helper TinyMCEHelper
+  end
 
+  module ClassMethods
+    def uses_tiny_mce(options = {})
+      tiny_mce_options = options.delete(:options) || {}
+      tiny_mce_options.reverse_merge!(:spellchecker_rpc_url => "/" + self.controller_name + "/spellchecker")
       proc = Proc.new do |c|
-        c.instance_variable_set(:@tiny_mce_options, tiny_mce_options.reverse_merge(:spellchecker_rpc_url => "/" + self.controller_name + "/spellchecker"))
+        c.instance_variable_set(:@tiny_mce_options, tiny_mce_options)
         c.instance_variable_set(:@uses_tiny_mce, true)
       end
       before_filter(proc, options)
 
       self.class_eval do
-	      def spellchecker
-	        return render :nothing => true if params[:params].blank? || params[:params][1].blank? || params[:params][0].blank? || params[:method].blank?
-	        puts "params: #{params.inspect}"
-	        headers["Content-Type"] = "text/plain"
-	        headers["charset"] = "utf-8"
-	        suggestions = check_spelling(params[:params][1], params[:method], params[:params][0])
-	        results = {"id" => nil, "result" => suggestions, "error" => nil}
-	        render :json => results
-	      end
-	
-			  def check_spelling(spell_check_text, command, lang)
-			    xml_response_values = Array.new
-			    spell_check_text = spell_check_text.join(' ') if command == 'checkWords'
-			    spell_check_response = `echo "#{spell_check_text}" | #{ASPELL_PATH} -a -l #{lang}`
-			    if (spell_check_response != '')
-			      spelling_errors = spell_check_response.split("\n").slice(1..-1)
-			      if (command == 'checkWords')
-			        for error in spelling_errors
-			          error.strip!
-			          if (match_data = error.match(ASPELL_WORD_DATA_REGEX))
-			            arr = match_data[0].split(' ')
-			            xml_response_values << arr[1]
-			          end 
-			        end 
-			      elsif (command == 'getSuggestions') 
-			        for error in spelling_errors 
-			          error.strip! 
-			          if (match_data = error.match(ASPELL_WORD_DATA_REGEX)) 
-			            xml_response_values << error.split(',')[1..-1].collect(&:strip!)
-			            xml_response_values = xml_response_values.first
-			          end
-			        end 
-			      end 
-			    end 
-			    return xml_response_values
-			  end 
+        include TinyMCE::SpellChecker
       end
     end
-
-    alias uses_text_editor uses_tiny_mce
-      
-    def self.included(recipient)
-      recipient.extend(InstanceMethods)
-    end
   end
-  
+
   module OptionValidator
     class << self
       cattr_accessor :plugins
-      
+
       def load
         @@valid_options = File.open(File.dirname(__FILE__) + "/../tiny_mce_options.yml") { |f| YAML.load(f.read) }
       end
-      
+
       def valid?(option)
-        @@valid_options.include?(option.to_s) || (plugins && plugins.include?(option.to_s.split('_')[0])) || option.to_s =~ /theme_advanced_container_/
+        option = option.to_s
+        @@valid_options.include?(option) || (plugins && plugins.include?(option.split('_')[0])) || option =~ /theme_advanced_container_/
       end
-    
+
       def options
         @@valid_options
       end
     end
   end
-  
 
+  module SpellChecker
+    require 'net/https'
+    require 'uri'
+    require 'rexml/document'
 
-  def self.included(base)
-    base.extend(ClassMethods)
-    base.helper TinyMCEHelper
+    ASPELL_WORD_DATA_REGEX = Regexp.new(/\&\s\w+\s\d+\s\d+(.*)$/)
+
+    # Attempt to dertermine where Aspell is
+    # Might be slow and a horrible way to do it, but it works!
+    ASPELL_PATH = if (`/usr/bin/aspell`) =~ /Usage/ # usually linux operating systems
+      "/usr/bin/aspell"
+    elsif (`/usr/local/bin/aspell`) =~ /Usage/ # usually mac operating systems
+      "/usr/local/bin/aspell"
+    else
+      "aspell" # this usally doesn't work depending on permissions but we'll fall back to it
+    end
+
+    def spellchecker
+      language, words, method = params[:params][0], params[:params][1], params[:method] unless params[:params].blank?
+      return render :nothing => true if language.blank? || words.blank? || method.blank?
+      headers["Content-Type"] = "text/plain"
+      headers["charset"] = "utf-8"
+      suggestions = check_spelling(words, method, language)
+      results = {"id" => nil, "result" => suggestions, "error" => nil}
+      render :json => results
+    end
+
+    def check_spelling(spell_check_text, command, lang)
+      xml_response_values = Array.new
+      spell_check_text = spell_check_text.join(' ') if command == 'checkWords'
+      logger.debug("Spellchecking via:  echo \"#{spell_check_text}\" | #{ASPELL_PATH} -a -l #{lang}")
+      spell_check_response = `echo "#{spell_check_text}" | #{ASPELL_PATH} -a -l #{lang}`
+      return xml_response_values if spell_check_response.blank?
+      spelling_errors = spell_check_response.split("\n").slice(1..-1)
+      for error in spelling_errors
+        error.strip!
+        if (match_data = error.match(ASPELL_WORD_DATA_REGEX))
+          if (command == 'checkWords')
+            arr = match_data[0].split(' ')
+            xml_response_values << arr[1]
+          elsif (command == 'getSuggestions')
+            xml_response_values << error.split(',')[1..-1].collect(&:strip!)
+            xml_response_values = xml_response_values.first
+          end
+        end
+      end
+      return xml_response_values
+    end
   end
+
 end
--- lib/tiny_mce_helper.rb
+++ lib/tiny_mce_helper.rb
@@ -1,5 +1,5 @@
 module TinyMCEHelper
-  class InvalidOption < Exception    
+  class InvalidOption < Exception
   end
 
   def using_tiny_mce?
@@ -13,10 +13,10 @@ module TinyMCEHelper
                         :theme => 'simple' }
     options = default_options.merge(options)
     TinyMCE::OptionValidator.plugins = options[:plugins]
+
     tinymce_js = "tinyMCE.init({\n"
-    i = 0    
-    options.stringify_keys.sort.each do |pair|
-      key, value = pair[0], pair[1]
+    options.stringify_keys.sort.each_with_index do |values, index|
+      key, value = values[0], values[1]
       raise InvalidOption.new("Invalid option #{key} passed to tinymce") unless TinyMCE::OptionValidator.valid?(key)
       tinymce_js += "#{key} : "
       case value
@@ -31,35 +31,27 @@ module TinyMCEHelper
       else
         raise InvalidOption.new("Invalid value of type #{value.class} passed for TinyMCE option #{key}")
       end
-      (i < options.size - 1) ? tinymce_js += ",\n" : "\n"
-      i += 1
+      (index < options.size - 1) ? tinymce_js += ",\n" : "\n"
     end
     tinymce_js += "\n});"
 	end
-  alias raw_tiny_mce raw_tiny_mce_init
 
-  def tiny_mce_init(options = @tiny_mce_options)
-    javascript_tag raw_tiny_mce_init(options)
+  def tiny_mce_init
+    javascript_tag raw_tiny_mce_init
   end
-  alias tiny_mce tiny_mce_init
-
   def tiny_mce_init_if_needed
-    tiny_mce_init if @uses_tiny_mce
+    tiny_mce_init if using_tiny_mce?
   end
-  alias tiny_mce_if_needed tiny_mce_init_if_needed
 
   def include_tiny_mce_js
     javascript_include_tag ((RAILS_ENV == 'development') ? "tiny_mce/tiny_mce_src" : "tiny_mce/tiny_mce")
   end
-  alias javascript_include_tiny_mce include_tiny_mce_js
-
   def include_tiny_mce_js_if_needed
-    include_tiny_mce_js if @uses_tiny_mce
+    include_tiny_mce_js if using_tiny_mce?
   end
-  alias javascript_include_tiny_mce_if_used include_tiny_mce_js_if_needed
 
   def include_tiny_mce_if_needed
-    if @uses_tiny_mce
+    if using_tiny_mce?
       include_tiny_mce_js + tiny_mce_init
     end
   end
