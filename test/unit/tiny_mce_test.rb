0ecba220fb4740a31c41ed7fdd2b87e9680e662b
Marian Theisen <marian@cice-online.net>
2010-01-04 14:24:41 +0100

    fixed 1 test

--- test/unit/tiny_mce_test.rb
+++ test/unit/tiny_mce_test.rb
@@ -5,12 +5,12 @@ class TinyMCETest < ActiveSupport::TestCase
   test "including tiny mce module into a class should provide uses_tiny_mce method" do
     test_controller = TestController.new
     assert !test_controller.class.respond_to?(:uses_tiny_mce)
-    TestController.send(:include, TinyMCE)
+    TestController.send(:include, TinyMCE::Base)
     assert test_controller.class.respond_to?(:uses_tiny_mce)
   end
 
   test "tiny mce plugin is included into action controller base and uses_tiny_mce method available" do
-    assert ActionController::Base::TinyMCE
+    assert ActionController::Base::TinyMCE::Base
     assert ApplicationController.respond_to?(:uses_tiny_mce)
   end
 
