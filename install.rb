21546e5f2b526bb89cd088f7fefe3c2fcd4d8c55
Kieran Pilkington <kieran@katipo.co.nz>
2010-01-06 11:18:58 +1300

    Revert "Install the TinyMCE Editor sources right away when installing as a plugin"
    This reverts commit d60c9295f2df64a0a0e0b75d4d28a4e57f8af929.
    
    Reason: Rails constant to get the public_path to install sources to, isn't available when the file is run.

--- install.rb
+++ install.rb
@@ -1,9 +1,8 @@
 # Lets output something to the console when people use script/plugin to install tiny_mce
-require File.join(File.dirname(__FILE__), 'init')
-
 puts "-------"
-TinyMCE.install_or_update_tinymce
+puts "tiny_mce plugin"
 puts ""
+puts "Thanks for installing the tiny_mce plugin."
 puts "To complete the process, please view README.rdoc."
 puts "Read the section entitled 'Installation"
 puts "-------"
