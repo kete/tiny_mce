57fea83dfb94347b86a7599e5075bede05d83b52
Marian Theisen <marian@cice-online.net>
2010-01-10 02:13:17 +0100

    all tests working again

--- test/unit/tiny_mce_test.rb
+++ test/unit/tiny_mce_test.rb
@@ -5,7 +5,7 @@ class TinyMCETest < ActiveSupport::TestCase
   test "including tiny mce module into a class should provide uses_tiny_mce method" do
     test_controller = TestController.new
     assert !test_controller.class.respond_to?(:uses_tiny_mce)
-    TestController.send(:include, TinyMCE)
+    TestController.send(:include, TinyMCE::Base)
     assert test_controller.class.respond_to?(:uses_tiny_mce)
   end
 
