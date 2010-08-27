f5bb73644328a094fa72f2360b0835f9e53a5ea3
Frederico Macedo <xanthobr@gmail.com>
2010-08-26 23:44:34 -0300

    organize task to a method on module TinyMCE

--- lib/tiny_mce.rb
+++ lib/tiny_mce.rb
@@ -68,6 +68,64 @@ module TinyMCE
       puts "Written configuration example to #{tiny_mce_yaml_filepath}"
     end
   end
+  
+  #this method generate new language to tinyMCE
+  def self.generate_new_lang(env_lang)
+    
+    puts "---------------------------------------------------------------------------------------"
+    puts "\t\t TinyMCE Language Basic Generator"
+    puts "---------------------------------------------------------------------------------------"
+    unless env_lang.empty?
+    
+      lib_tinymce = File.join(File.dirname(__FILE__), 'tiny_mce')
+      file = File.join(lib_tinymce, "valid_tinymce_langs.yml")
+      yml_langs = YAML::load(File.open(file))
+
+      unless yml_langs.include?(env_lang)
+        yml_langs << env_lang
+        yml_langs.sort!
+    
+        #writing new language to valid_tinymce_langs.yml
+        File.open(file, 'w') do |f|
+          f << "#\n# For more information about available languages, see\n"
+          f <<  "# http://tinymce.moxiecode.com/download_i18n.php\n"
+          f << "# Should only include a list of completed translations (not incomplete ones which most are :-( )\n#\n\n"
+          f << YAML.dump(yml_langs).to_s
+        end
+         #start to copy en files to new lang
+          puts "Generated \"en\" lang copies, translate this files:"
+          puts "---------------------------------------------------------------------------------------"
+          puts " REMEMBER TO CHANGE ALL THIS FILES."
+          puts "IF YOU WANT TO TRANSLATE AN ESPECIFIC PLUGIN GO TO plugin/langs AND ADD YOUR LANG\n\n"
+        
+      else
+        puts "\n\n\t\tLanguage exists on configuration file.\n\n" 
+      end
+    
+      assets_path = File.join(lib_tinymce, 'assets', 'tiny_mce') 
+      unless File.exists?(File.join(assets_path, 'langs',"#{env_lang}.js"))
+        puts "\t- tiny_mce/lib/tiny_mce/assets/tiny_mce/langs/#{env_lang}.js"
+        FileUtils.cp( File.join(assets_path, "langs", "en.js"), File.join(assets_path, "langs","#{env_lang}.js")) 
+      end
+      unless File.exists?(File.join(assets_path,"themes","advanced","langs","#{env_lang}.js"))
+        puts "\t- tiny_mce/lib/tiny_mce/assets/tiny_mce/themes/advanced/langs/#{env_lang}.js"
+        FileUtils.cp(File.join(assets_path,"themes","advanced","langs","en.js"), File.join(assets_path,"themes","advanced","langs","#{env_lang}.js")) 
+      end
+      unless File.exists?(File.join(assets_path,"themes","advanced","langs","#{env_lang}_dlg.js"))
+        puts "\t- tiny_mce/lib/tiny_mce/assets/tiny_mce/themes/advanced/langs/#{env_lang}_dlg.js"
+        FileUtils.cp(File.join(assets_path,"themes","advanced","langs","en_dlg.js"), File.join(assets_path,"themes","advanced","langs","#{env_lang}_dlg.js"))
+      end
+      unless File.exists?(File.join(assets_path,"themes","simple","langs","#{env_lang}.js"))
+        puts "\t- tiny_mce/lib/tiny_mce/assets/tiny_mce/themes/simple/langs/#{env_lang}_dlg.js"
+        FileUtils.cp(File.join(assets_path,"themes","simple","langs","en.js"), File.join(assets_path,"themes","simple","langs","#{env_lang}.js")) 
+      end
+    
+      puts "---------------------------------------------------------------------------------------"
+    
+    else
+      puts "You must set the LANG environment. example: rake tiny_mce:new:lang LANG=pt-BR"
+    end
+  end
 
   module Base
     include TinyMCE::SpellChecker
