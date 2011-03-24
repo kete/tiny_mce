ed5f7a40113a700e56da6b3bd740b2956cafe7ab
Sasha Gerrand <github.com-kete-tiny_mce@sgerrand.com>
2011-03-24 20:24:19 +1100

    - Updated TinyMCE assets to version 3.4 - Added new complete language translations and updated those previously installed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + "/../../../../test/test_helper")
+require File.expand_path(File.dirname(__FILE__) + "/../test/test_helper")
 
 # Use non-default action names to get around possible authentication
 # filters to ensure the tests work in most cases
