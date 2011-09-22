ebe4731ea6a9e119e32dc8de2ee63fc6536230ea
Zac Zheng <zac@zenpow.com>
2011-09-22 16:30:47 +0100

    fixed plugin configuration double quote encoding issue for rails 3.0

--- lib/tiny_mce/configuration.rb
+++ lib/tiny_mce/configuration.rb
@@ -82,7 +82,7 @@ module TinyMCE
         when String, Symbol, Fixnum
           "'#{value.to_s}'"
         when Array
-          '"' + value.join(',') + '"'
+          "'" + value.join(',') + "'"
         when TrueClass
           'true'
         when FalseClass
